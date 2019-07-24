ddddbf6a7526cd04a7a50eb8e17e12c31a83be1e
Swapnil Shinde <FWIN01798@fulcrumww.com>
2019-07-24 15:07:13 +0530

    swapnil: initial commit done

---
+++ TextToSpeechConverter/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TextToSpeechConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_Speak = new System.Windows.Forms.Button();
+            this.btn_Resume = new System.Windows.Forms.Button();
+            this.btn_Pause = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(414, 193);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_Speak
+            // 
+            this.btn_Speak.Location = new System.Drawing.Point(12, 220);
+            this.btn_Speak.Name = "btn_Speak";
+            this.btn_Speak.Size = new System.Drawing.Size(79, 29);
+            this.btn_Speak.TabIndex = 1;
+            this.btn_Speak.Text = "Speak";
+            this.btn_Speak.UseVisualStyleBackColor = true;
+            this.btn_Speak.Click += new System.EventHandler(this.btn_Speak_Click);
+            // 
+            // btn_Resume
+            // 
+            this.btn_Resume.Location = new System.Drawing.Point(97, 220);
+            this.btn_Resume.Name = "btn_Resume";
+            this.btn_Resume.Size = new System.Drawing.Size(77, 29);
+            this.btn_Resume.TabIndex = 2;
+            this.btn_Resume.Text = "Resume";
+            this.btn_Resume.UseVisualStyleBackColor = true;
+            this.btn_Resume.Click += new System.EventHandler(this.btn_Resume_Click);
+            // 
+            // btn_Pause
+            // 
+            this.btn_Pause.Location = new System.Drawing.Point(180, 220);
+            this.btn_Pause.Name = "btn_Pause";
+            this.btn_Pause.Size = new System.Drawing.Size(88, 29);
+            this.btn_Pause.TabIndex = 3;
+            this.btn_Pause.Text = "Pause";
+            this.btn_Pause.UseVisualStyleBackColor = true;
+            this.btn_Pause.Click += new System.EventHandler(this.btn_Pause_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(274, 220);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(79, 29);
+            this.btn_Stop.TabIndex = 4;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 261);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Pause);
+            this.Controls.Add(this.btn_Resume);
+            this.Controls.Add(this.btn_Speak);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Text to Speech Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_Speak;
+        private System.Windows.Forms.Button btn_Resume;
+        private System.Windows.Forms.Button btn_Pause;
+        private System.Windows.Forms.Button btn_Stop;
+    }
+}
+
